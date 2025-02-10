6b92059e24d109fd0ea9bedebef23a9fc902601f
raj aishwary pratap singh <rajaishwaryasingh@gmail.com>
2025-02-10 23:39:20 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
