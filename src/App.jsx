87d06522d02292b7ba49af08c61643d580d6f645
raj aishwary pratap singh <rajaishwaryasingh@gmail.com>
2025-02-11 16:19:10 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -10,8 +11,11 @@ function App() {
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<Dashboard />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
 
 export default App;
+
+
